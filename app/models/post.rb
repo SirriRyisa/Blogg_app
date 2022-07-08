7b764115dc69d41bcdf5babd8ca9d24bcf69e34e
SirriRyisa <sirriryisa@gmail.com>
2022-07-08 09:51:34 +0100

    Added new methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   has_many :likes
 
   def recent_comments
-    comments.order(created_at: :desc).limit(10)
+    comments.order(created_at: :desc).limit(5)
   end
 
   def posts_count
